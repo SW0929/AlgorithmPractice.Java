bde0be04a4d6969ed5d3a58f4a6939bbdb11096a
sw <sjugas1234@naver.com>
2022-08-05 19:28:37 +0900

    20220805-01

---
+++ src/d01/Main.java
@@ -0,0 +1,27 @@
+package d01;
+//대문자는 소문자로, 소문자는 대문자로 변환
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String s = sc.nextLine();
+		char a[] = new char[s.length()];
+		
+		for(int i = 0; i < a.length; i++) {
+			
+			if(s.charAt(i) >= 'a' && s.charAt(i) <= 'z' ) {
+				a[i] = (char)(s.charAt(i) + 'A'-'a');
+			}
+			else {
+				a[i] = (char)(s.charAt(i) - ('A'-'a'));
+			}
+		}
+		System.out.print(a);
+		
+		sc.close();
+	}
+
+}
