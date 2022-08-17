5da8fc512dea2ce104b92ab3ce873161ce4d06a0
sw <sjugas1234@naver.com>
2022-08-17 12:02:44 +0900

    20220817-01

--- src/o01/Main.java
+++ src/o01/Main.java
@@ -1,6 +1,9 @@
 package o01;
 //입력된 두 세 자리 수 중 거꾸로 읽었을 떄 큰 수를 출력하시오.
 
+
+//아직 덜 풀었음
+//git config --global core.autocrif true
 import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
@@ -15,16 +18,19 @@ public class Main {
 		
 		String n1 = st.nextToken();
 		String n2 = st.nextToken();
-		int m1[] = new int[6];
+		String m1 = "";
+		String m2 = "";
+		for(int i = 2; i >=0; i--) {
+			m1 += n1.charAt(i);
+			m2 += n2.charAt(i);
+		}
+		if(Integer.parseInt(m1) > Integer.parseInt(m2))
+			System.out.println(m1);
+		else
+			System.out.println(m2);
 		
 		
-		for(int i = 0; i < 3; i++) {
-			m1[i] = n1.charAt(i)-'0';
-			m1[i+3] = n2.charAt(i)-'0';
-		}
-		for(int i = 0; i < m1.length; i++) {
-			System.out.println(m1[i]);
-		}
+		
 		
 		
 	}
